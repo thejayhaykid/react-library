28e06eba430b1e74bc58b80ef861725205a40803
Jake <jayhay28324@gmail.com>
2020-01-24 09:08:19 -0500

    Added start to backend

--- src/router/index.js
+++ src/router/index.js
@@ -15,24 +15,12 @@ import {
               <li>
                 <Link to="/">Home</Link>
               </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
             </ul>
           </nav>
   
           {/* A <Switch> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
           <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
             <Route path="/">
               <Home />
             </Route>
